c76e554ea657f997a7aa0671ccaf9e4c067501e3
Agnieszka Lis <agnieszkalis@gmail.com>
2017-09-12 16:06:57 +0100

    Added named-cat path

--- app.rb
+++ app.rb
@@ -17,7 +17,14 @@ get '/goodbye' do
   "see yah"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @color = params[:color]
   erb :index
 end
