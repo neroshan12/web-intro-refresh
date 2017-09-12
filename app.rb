ae8c29c2b9e3c84b9d79d09f00736567f6b76478
Nero Siva <neroshan12@gmail.com>
2017-09-12 15:05:39 +0100

    Add Sinatra - Start & Restart

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Secret message"
+end
+
+get '/test' do
+  "test!"
+end
+
+get '/goodbye' do
+  "see yah"
+end
